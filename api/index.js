9f97d35392ac4d31cc4563a50f930251086dc51c
Onah Ben <onnassiz@gmail.com>
2018-04-26 18:42:47 +0100

    Feature: Added mock json data for users

--- api/index.js
+++ api/index.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const apiRouter = express.Router();
 const app = express();
 
 
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+
 app.use(bodyParser.json());
 
+
 app.get('/api', (req, res) => res.send('Welcome to Book-A-Meal'));
+
+app.use('/api/v1', apiRouter);
+
+
 app.listen(3002, () => console.log('App running at port 3002.'));
