4eb75c3fb4c6ec65ed938c149b7a25f060ae8ecf
Onah Ben <onnassiz@gmail.com>
2018-05-16 12:37:33 +0100

    Fix: Resolved travis issues

--- api/models/index.js
+++ api/models/index.js
@@ -7,8 +7,8 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
-// var config    = require(__dirname + '/../config/config.json')[env];
-var config    = require(path.resolve('api/config/config.js'))[env];
+var config    = require(__dirname + '/../config/config.json')[env];
+// var config    = require(path.resolve('api/config/config.js'))[env];
 var db        = {};
 
 var sequelize = config.use_env_variable ? new Sequelize(process.env[config.use_env_variable], config) : new Sequelize(config.database, config.username, config.password, config);
